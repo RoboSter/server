51b7194d66b8ce969dd14a95286f0fb8f152d02f
bymse <egorstrelov@gmail.com>
2020-04-07 21:22:17 +0300

    issues/9 Additional helpers + json utilities folder + remove example worker

--- src/RoboSter.Utilities/Helpers/TypeHelper.cs
+++ src/RoboSter.Utilities/Helpers/TypeHelper.cs
@@ -6,6 +6,9 @@ namespace RoboSter.Utilities.Helpers
     public static class TypeHelper
     {
         public static bool HasAttribute<T>(this Type type) where T : Attribute =>
-            type.GetCustomAttribute(typeof(T)) != null;
+            Attribute.IsDefined(type, typeof(T));
+
+        public static bool HasAttribute<T>(this MemberInfo info) where T : Attribute =>
+            Attribute.IsDefined(info, typeof(T));
     }
 }
